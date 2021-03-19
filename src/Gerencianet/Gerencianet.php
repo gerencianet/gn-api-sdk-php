2d159af3df32e4fcab88bcbe3ff970796361be28
Guilherme Cota <guilherme.cota@gerencianet.com.br>
2021-03-19 13:46:06 -0300

    Added function to change the endpoint file.

--- src/Gerencianet/Gerencianet.php
+++ src/Gerencianet/Gerencianet.php
@@ -4,4 +4,12 @@ namespace Gerencianet;
 
 class Gerencianet extends Endpoints
 {
+    public function __construct($options, $requester = null, $endpointsConfigFile = null)
+    {
+        if ($endpointsConfigFile) {
+            Config::setEndpointsConfigFile($endpointsConfigFile);
+        }
+
+        parent::__construct($options, $requester);
+    }
 }
