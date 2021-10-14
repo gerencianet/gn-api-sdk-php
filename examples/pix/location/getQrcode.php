d2976a8a49f33b277c11bbd077b27f1619e5673a
Guilherme Cota <75252715+guilhermecotaGn@users.noreply.github.com>
2021-10-14 15:03:49 -0300

    Criado arquivo de exemplo para buscar QR Code

---
+++ examples/pix/location/getQrcode.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use Gerencianet\Exception\GerencianetException;
+use Gerencianet\Gerencianet;
+
+$file = file_get_contents(__DIR__ . '/../../config.json');
+$options = json_decode($file, true);
+
+$params = ['id' => ''];
+
+try {
+    $api = Gerencianet::getInstance($options);
+    $pix = $api->pixGenerateQRCode($params);
+
+    echo 'Pix Copia e Cola e QR Code:<br>';
+    echo '<pre>' . json_encode($pix, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . '</pre>';
+    
+    echo 'Imagem:<br />';
+    echo '<img src="' . $pix['imagemQrcode'] . '" />';
+} catch (GerencianetException $e) {
+    print_r($e->code);
+    print_r($e->error);
+    print_r($e->errorDescription);
+
+    throw new Error($e->error);
+} catch (Exception $e) {
+    throw new Error($e->getMessage());
+}
