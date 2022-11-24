6b315d230544f7cb48e946263e699ed06c5ba7c8
guilhermecotaGn <75252715+guilhermecotaGn@users.noreply.github.com>
2022-11-24 14:38:44 -0300

    Renamed method
    Method "pixLocationGet()" renamed to "pixDetailLocation()"

---
+++ examples/pix/location/pixDetailLocation.php
@@ -0,0 +1,31 @@
+<?php
+
+if (file_exists($autoload = realpath(__DIR__ . "/../../../vendor/autoload.php"))) {
+	require_once $autoload;
+} else {
+	print_r("Autoload not found or on path <code>$autoload</code>");
+}
+
+use Gerencianet\Exception\GerencianetException;
+use Gerencianet\Gerencianet;
+
+if (file_exists($options = realpath(__DIR__ . "/../../credentials/options.php"))) {
+	require_once $options;
+}
+
+$params = [
+	"id" => 0
+];
+
+try {
+	$api = Gerencianet::getInstance($options);
+	$response = $api->pixDetailLocation($params);
+
+	print_r("<pre>" . json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+} catch (GerencianetException $e) {
+	print_r($e->code);
+	print_r($e->error);
+	print_r($e->errorDescription);
+} catch (Exception $e) {
+	print_r($e->getMessage());
+}
