372e249a2855e8423120d088eb203b4d6587b182
guilhermecotaGn <75252715+guilhermecotaGn@users.noreply.github.com>
2022-11-24 14:36:54 -0300

    Renamed method
    Method "paySubscription()" renamed to "defineSubscriptionPayMethod()"

---
+++ examples/charges/subscription/defineSubscriptionCard.php
@@ -0,0 +1,60 @@
+<?php
+
+if (file_exists($autoload = realpath(__DIR__ . "/../../../vendor/autoload.php"))) {
+	require_once $autoload;
+} else {
+	print_r("Autoload not found or on path <code>$autoload</code>");
+}
+
+use Gerencianet\Exception\GerencianetException;
+use Gerencianet\Gerencianet;
+
+if (file_exists($options = realpath(__DIR__ . "/../../credentials/options.php"))) {
+	require_once $options;
+}
+
+$params = [
+	"id" => 0 // subscription id
+];
+
+$paymentToken = "insert_here_the_payment_token_referring_to_card_data";
+
+$customer = [
+	"name" => "Gorbadoc Oldbuck",
+	"cpf" => "04267484171",
+	"phone_number" => "5144916523",
+	"email" => "oldbuck@gerencianet.com.br",
+	"birth" => "1977-01-15"
+];
+
+$billingAddress = [
+	"street" => "Av. JK",
+	"number" => 909,
+	"neighborhood" => "Bauxita",
+	"zipcode" => "35400000",
+	"city" => "Ouro Preto",
+	"state" => "MG",
+];
+
+$body = [
+	"payment" => [
+		"credit_card" => [
+			"billing_address" => $billingAddress,
+			"payment_token" => $paymentToken,
+			"customer" => $customer
+		]
+	]
+];
+
+try {
+	$api = new Gerencianet($options);
+	$response = $api->defineSubscriptionPayMethod($params, $body);
+
+	print_r("<pre>" . json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+} catch (GerencianetException $e) {
+	print_r($e->code);
+	print_r($e->error);
+	print_r($e->errorDescription);
+} catch (Exception $e) {
+	print_r($e->getMessage());
+}
