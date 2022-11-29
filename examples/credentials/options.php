b69d621f913a60b65d4c58642314fc1d52f3dc7f
Guilherme Cota <75252715+guilhermecotaGn@users.noreply.github.com>
2022-11-29 13:52:21 -0300

    Update options.php

--- examples/credentials/options.php
+++ examples/credentials/options.php
@@ -3,7 +3,7 @@
 $options = [
 	"client_id" => "Client_Id",
 	"client_secret" => "Client_Secret",
-	"certificate" => realpath("/vagrant/www/gn-api-sdk-php/examples/credentials/productionCertificate.p12"), // Absolute path to the certificate in .pem or .p12 format
+	"certificate" => realpath(__DIR__ . "/productionCertificate.p12"), // Absolute path to the certificate in .pem or .p12 format
 	"sandbox" => false,
 	"debug" => false,
 	"timeout" => 30
